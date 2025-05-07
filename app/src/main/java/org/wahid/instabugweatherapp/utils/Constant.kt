ae9147ef3d835b72058d02caa6b5506d86985034
Wahid Ali <1213089@dtu.edu.eg>
2025-05-07 08:51:38 +0300

    Add Constant

---
+++ app/src/main/java/org/wahid/instabugweatherapp/utils/Constant.kt
@@ -0,0 +1,8 @@
+package org.wahid.instabugweatherapp.utils
+
+object Constant {
+    const val SHARED_PREFS_NAME = "CACHE_DETAILS"
+    const val CACHE_TIME_OUT_KEY = "cache_time_out"
+    const val INIT_LAUNCH_KEY = "init_launch"
+
+}
