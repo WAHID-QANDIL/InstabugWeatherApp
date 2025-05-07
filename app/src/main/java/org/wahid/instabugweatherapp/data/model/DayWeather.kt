b98930dc7af5f90132ab06ed72e570aeff31c48a
Wahid Ali <1213089@dtu.edu.eg>
2025-05-07 08:52:47 +0300

    Refactor the data mode class, and add mapper method to convert it to database entity

--- app/src/main/java/org/wahid/instabugweatherapp/data/model/DayWeather.kt
+++ app/src/main/java/org/wahid/instabugweatherapp/data/model/DayWeather.kt
@@ -1,14 +1,33 @@
 package org.wahid.instabugweatherapp.data.model
 
+import org.wahid.instabugweatherapp.data.local.db.model.WeatherDbEntity
+
+
 data class DayWeather(
-    val date: String,
-    val tempMax: Double,
-    val tempMin: Double,
-    val temp: Double,
-    val humidity: Double,
-    val precip: Double,
-    val windSpeed: Double,
-    val pressure: Double,
-    val description: String,
-    val icon: String?
-)
+    val date:           String,
+    val tempMax:        Double,
+    val tempMin:        Double,
+    val temp:           Double,
+    val humidity:       Double,
+    val precip:         Double,
+    val windSpeed:      Double,
+    val pressure:       Double,
+    val description:    String,
+//    val address:        String, all days belongs to the same location/address
+){
+    fun toDb(): WeatherDbEntity{
+        return WeatherDbEntity(
+            date        = date,
+            tempMax     = tempMax,
+            tempMin     = tempMin,
+            temp        = temp,
+            humidity    = humidity,
+            precip      = precip,
+            windSpeed   = windSpeed,
+            pressure    = pressure,
+            description = description,
+            address     = "UNKNOWN",
+            lastUpdate  = System.currentTimeMillis()
+        )
+    }
+}
