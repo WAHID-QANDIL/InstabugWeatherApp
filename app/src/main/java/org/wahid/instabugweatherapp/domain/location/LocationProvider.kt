7c49464756736f23d6c42ee21bf504aa5fde2f1c
Wahid Ali <1213089@dtu.edu.eg>
2025-05-09 09:35:45 +0300

    Add location provider to get the current device location

---
+++ app/src/main/java/org/wahid/instabugweatherapp/domain/location/LocationProvider.kt
@@ -0,0 +1,61 @@
+package org.wahid.instabugweatherapp.domain.location
+
+import android.Manifest.permission
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import androidx.annotation.RequiresPermission
+import org.wahid.instabugweatherapp.utils.AppContainer
+import org.wahid.instabugweatherapp.utils.AppExecutors
+
+class LocationProvider(
+    context: Context = AppContainer.appContext,
+    private val minTimeMs: Long = 5_000L,
+    private val minDistanceM: Float = 10f,
+) {
+    val locationManager = (context.getSystemService(Context.LOCATION_SERVICE)) as LocationManager
+    private var locationListener: LocationListener? = null
+
+
+    @RequiresPermission(allOf = [permission.ACCESS_FINE_LOCATION, permission.ACCESS_COARSE_LOCATION])
+    fun startListening(
+        onLocation: (Location) -> Unit,
+    ): Boolean {
+
+        val provider = when {
+            locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) -> LocationManager.GPS_PROVIDER
+            locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) -> LocationManager.NETWORK_PROVIDER
+            else -> throw IllegalArgumentException("No location provider enabled")
+        }
+        stopListening()
+        locationListener = object : LocationListener {
+            override fun onLocationChanged(loc: Location) = onLocation(loc)
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+            override fun onProviderEnabled(provider: String) {}
+            override fun onProviderDisabled(provider: String) {
+                throw Exception("The GPS provider is not enabled")
+            }
+        }
+
+
+        locationManager.requestLocationUpdates(
+            provider,
+            minTimeMs,
+            minDistanceM,
+            locationListener!!,
+            AppExecutors.locationLooper
+        )
+        return true
+
+
+    }
+
+    fun stopListening() {
+        locationListener?.let {
+            locationManager.removeUpdates(it)
+            locationListener = null
+        }
+    }
+}
